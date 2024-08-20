85023a559cc77c1394ce9e359b6ea93f15ca75f1
Serenasing <serenasing@uvic.ca>
2024-08-19 18:16:08 -0700

    first commit Base for flashcard app

---
+++ js/app.js
@@ -0,0 +1,29 @@
+const term = document.querySelector(".term");
+const definition = document.querySelector(".definition");
+const checkButton= document.querySelector(".check");
+const nextButton= document.querySelector(".next");
+
+const words = {
+    one: "1",
+    two: "2",
+    three: "3",
+};
+
+const data = Object.entries(words);
+
+function getRandomTerm(){
+    const randomTerm = data[Math.floor(Math.random() * data.length)];
+    term.innerHTML = `<h3>${randomTerm[0]}</h3>`;
+    definition.innerHTML = `<h3>${randomTerm[1]}</h3>`; 
+};
+
+
+checkButton.addEventListener('click', function() {
+    definition.style.display = 'block';
+});
+
+nextButton.addEventListener('click', function() {
+    definition.style.display = 'none';
+    getRandomTerm();
+});
+
